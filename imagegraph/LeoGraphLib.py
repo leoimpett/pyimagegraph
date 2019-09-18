9d58efe97d63ecb5fd40ec0e044356fadd542f84
Leonardo Impett <leoimpett@googlemail.com>
2019-09-18 15:49:45 +0200

    auto updated

--- imagegraph/LeoGraphLib.py
+++ imagegraph/LeoGraphLib.py
@@ -31,9 +31,10 @@ def loadGNM(nImages='500'):
 		csvreader = csv.reader(myfile)
 		for row in csvreader:
 			mycsv.append(row)
-	shuffledCSV = np.random.shuffle(mycsv) # shuffle the image database
+	print('Shuffling image database...')
+	np.random.shuffle(mycsv) # shuffle the image database
 	imageList = []
-	for row in tqdm.tqdm(shuffledCSV[:nImages]):    
+	for row in tqdm.tqdm(mycsv[:nImages]):    
 		thisimage = io.imread(row[0].split(';')[0])
 		if len(thisimage.shape) != 3:
 			thisimage = color.gray2rgb(thisimage)
